2bb536bbe0fc014b03290cb9e6ee4e461aad1bb0
Jerry Catron <catronjer@gmail.com>
2021-08-28 15:54:34 -0400

    fixed spotify top tracks api call

--- pages/api/top-tracks.js
+++ pages/api/top-tracks.js
@@ -3,10 +3,10 @@ import { getTopTracks } from '../../lib/spotify';
 export default async function handler(_, res) {
   const response = await getTopTracks();
   const { items } = await response.json();
-
   const tracks = items.slice(0, 10).map((track) => ({
     artist: track.artists.map((_artist) => _artist.name).join(', '),
     songUrl: track.external_urls.spotify,
+    albumImageUrl: track.album.images[0].url,
     title: track.name
   }));
 
