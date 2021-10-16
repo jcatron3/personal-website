ceb561875e1beffbaa2b05a62170586cb45053df
Jerry Catron <catronjer@gmail.com>
2021-10-16 16:33:11 -0400

    vercel sucks

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ csp += `form-action 'self';`
 csp += `default-src 'self';`
 csp += `script-src 'self' 'unsafe-inline' ${prod ? "" : "'unsafe-eval'"};`                    // NextJS requires 'unsafe-eval' in dev (faster source maps)
 csp += `style-src 'self' https://fonts.googleapis.com 'unsafe-inline' data:;` // NextJS requires 'unsafe-inline'
-csp += `img-src 'self' https://*.githubusercontent.com https://i.scdn.co/image/*  data: blob:;`
+csp += `img-src 'self' https://*.githubusercontent.com https://i.scdn.co  data: blob:;`
 csp += `font-src 'self' https://fonts.gstatic.com;`  // TODO
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
