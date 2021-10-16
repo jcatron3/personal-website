eb978d9e01b1e820f465d342bed4b88f8a029327
Jerry Catron <catronjer@gmail.com>
2021-10-16 14:48:47 -0400

    switched back to Image comps and added security headers to next.config.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -5,7 +5,7 @@ import TopTracks from "../components/topTracks.js";
 
 export default function Dashboard() {
   return (
-    <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
+    <div className="flex flex-col justify-center items-start max-w-2xl mx-2 mb-16">
       <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
         Dashboard
       </h1>
