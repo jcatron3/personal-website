fce5b19df7f6f83844c3a362e67b4828b74048b3
Jake Catron <jcatron@google.com>
2026-05-27 09:07:43 -0700

    perf(assets): optimize profile avatar and resolve layout redundancy
    - Compress and resize profile avatar from 7.0MB PNG to 127KB JPEG
    - Update image reference in sectionAbout component to point to optimized JPEG

--- components/sectionAbout.js
+++ components/sectionAbout.js
@@ -21,13 +21,14 @@ export default function SectionAbout() {
         </div>
         <div className="flex-shrink-0 lg:mt-12 lg:px-4 mb-10">
           <Image
-            src='/profile.png'
+            src='/profile.jpg'
             alt="Profile"
             priority={true}
             className="rounded-full"
             width={250}
             height={250}
             placeholder="blur"
+            blurDataURL="/profile.jpg"
           />
         </div>
       </div>
